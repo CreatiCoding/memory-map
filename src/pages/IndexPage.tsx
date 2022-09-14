33fd5c9cb5f5e4014405418a7d5aea1688616583
seokho-jeong <seokho.jeong@toss.im>
2022-09-14 12:04:27 +0900

    fix(pageSize): 기본 20개씩 보이도록 변경

--- src/pages/IndexPage.tsx
+++ src/pages/IndexPage.tsx
@@ -1,16 +1,24 @@
 import styled from "@emotion/styled";
+import { useRouter } from "next/router";
 import { CardList } from "../components/CardList";
 import { Txt } from "../components/Txt";
 import { margin, width } from "../utils/css";
 
 export function IndexPage() {
+  const router = useRouter();
+
   return (
     <Main>
       <br />
       <Title center>Memory Map</Title>
       <br />
       <br />
-      <CardList category="coding-architecture" />
+      <CardList
+        category="coding-architecture"
+        pageSize={
+          router.query.pageSize != null ? Number(router.query.pageSize) : 20
+        }
+      />
     </Main>
   );
 }
