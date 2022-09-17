841732ff354ec637fe2691e660ae0ebf1a453172
정석호 <seokho.jeong@toss.im>
2022-09-17 22:47:08 +0900

    padding 추가

--- src/utils/css.ts
+++ src/utils/css.ts
@@ -1,20 +1,66 @@
 import { css } from "@emotion/react";
 
-const margin = (px: number) => {
-  return css`
-    margin: ${px}px;
-  `;
+const margin = (px: number, x?: number) => {
+  if (x == null) {
+    return css`
+      margin: ${px}px;
+    `;
+  } else {
+    return css`
+      margin: ${px}px ${x}px;
+    `;
+  }
 };
 
 margin.x = (px: number) => {
   return css`
-    margin: 0 ${px}px 0 ${px}px;
+    margin-left: ${px}px;
+    margin-right: ${px}px;
   `;
 };
 
 margin.y = (px: number) => {
   return css`
-    margin: ${px}px 0 ${px}px 0;
+    margin-top: ${px}px;
+    margin-bottom: ${px}px;
+  `;
+};
+
+const padding = (px: number, x?: number) => {
+  if (x == null) {
+    return css`
+      padding: ${px}px;
+    `;
+  } else {
+    return css`
+      padding: ${px}px ${x}px;
+    `;
+  }
+};
+
+padding.top = (px: number) => css`
+  padding-top: ${px}px;
+`;
+padding.bottom = (px: number) => css`
+  padding-bottom: ${px}px;
+`;
+padding.left = (px: number) => css`
+  padding-left: ${px}px;
+`;
+padding.right = (px: number) => css`
+  padding-right: ${px}px;
+`;
+padding.x = (px: number) => {
+  return css`
+    padding-left: ${px}px;
+    padding-right: ${px}px;
+  `;
+};
+
+padding.y = (px: number) => {
+  return css`
+    padding-top: ${px}px;
+    padding-bottom: ${px}px;
   `;
 };
 
@@ -42,8 +88,8 @@ height.percent = (percent: number) => {
   `;
 };
 
-export { margin, width };
-
-const cssUtils = { margin, width, height };
+const cssUtils = { margin, padding, width, height };
 
 export default cssUtils;
+
+export { margin, padding, width, height };
