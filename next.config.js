92e3bf055878084f139da67707eebcac9f6a1ec2
정석호 <seokho.jeong@toss.im>
2022-09-18 00:03:19 +0900

    next.config 주석 추가

--- next.config.js
+++ next.config.js
@@ -2,6 +2,13 @@
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
-}
+  // TODO: CI로 분리한 후, 실제 배포할 때는 주석을 풀어 체크를 넘어가자
+  // typescript: {
+  //   ignoreBuildErrors: true,
+  // },
+  // eslint: {
+  //   ignoreDuringBuilds: true,
+  // },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
